98dfc2982311ac1a9e0e660af8556e06f63e7af2
DanielViera98 <dviera98@gmail.com>
2023-10-18 20:51:39 -0400

    Update model

---
+++ Migrations/20231019004429_InitialCreate.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Event_Manager.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    Website = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    RentalFee = table.Column<float>(type: "real", nullable: false),
+                    VendorCapacity = table.Column<int>(type: "integer", nullable: false),
+                    AttendeeCapacity = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.Address);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    EventId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Website = table.Column<string>(type: "text", nullable: false),
+                    LocationAddress = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.EventId);
+                    table.ForeignKey(
+                        name: "FK_Events_Locations_LocationAddress",
+                        column: x => x.LocationAddress,
+                        principalTable: "Locations",
+                        principalColumn: "Address",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_LocationAddress",
+                table: "Events",
+                column: "LocationAddress");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events");
+
+            migrationBuilder.DropTable(
+                name: "Locations");
+        }
+    }
+}
--- Model.cs
+++ Model.cs
@@ -13,6 +13,7 @@ using System.Reflection;
 public class EventContext : DbContext
 {
     public DbSet<Event> Events { get; set; }    //Table containing Stock classes
+    public DbSet<Location> Locations { get; set; }    //Table containing Stock classes
     public string DbPath { get; }               //Path to the database
     public string StockPath { get; set; }       //Path to the Stock Files folder
 
@@ -22,18 +23,6 @@ public class EventContext : DbContext
         var path = Environment.GetFolderPath(folder);                   //Get path to folder
         DbPath = System.IO.Path.Join(path, "Stocks.db");                //Update path variable to folder + name for database
         string mypath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);    //Get path to exe location
-        if (mypath != null)
-        {
-            //Get substring of path to solution folder, then add "\Stock Data". Save as StockPath.
-            mypath = mypath.Substring(0, mypath.IndexOf("COP4365_Project1") + "COP4365_Project1".Length);
-            mypath += "\\Stock Data";
-            StockPath = mypath;
-        }
-        else
-        {
-            Debug.WriteLine("FAILED TO GET CURRENT DIRECTORY");
-            return;
-        }
     }
     // The following configures EF to create a Sqlite database file in the
     // special "local" folder for your platform.
@@ -48,17 +37,14 @@ public class EventContext : DbContext
 public class Event                             //Stock class containing all information for stock
 {
     //Ignore so that the csvReader does not try and use it as a column to read into. Key marks as unique identifier for entry.
-    [Key] public Guid Id { get; set; }              //Identifier for Stock entry.
-    public DateOnly Date { get; set; }                      //Date of stock (DateTime format)
-    public string Ticker { get; set; }                      //Stock Ticker (Three/four letter string)
-    public string Period { get; set; }                      //Timespan for the stock (month/week/day)
-    public double Open { get; set; }                        //Open value for stock (Dollars-Cents)
-    public double High { get; set; }                        //High value for stock (Dollars-Cents)
-    public double Low { get; set; }                         //Low value for stock (Dollars-Cents)
-    public double Close { get; set; }                       //Close value for stock (Dollars-Cents)
-    //Needs to be 64-bit, numbers get too large for signed 32-bit
-    public Int64 Volume { get; set; }                       //Volume value for stock (Int count)
-    public string StockFilePath { get; set; }      //Path to file
+    [Key] public Guid EventId { get; set; }              //Identifier for Stock entry.
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string Website {  get; set; }
+    public Location Location { get; set; }
+
 }
 
 public class Location
@@ -69,4 +55,5 @@ public class Location
     public float RentalFee { get; set; }
     public int VendorCapacity { get; set; }
     public int AttendeeCapacity { get; set; }
+    public List<Event> Events { get; set; }
 }
