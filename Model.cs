ef8a02f6dfc742e7e247322fcfd4fab0c75ed7b7
HeWhoWalksBefore <dviera@usf.edu>
2023-11-01 18:01:03 -0400

    Added the remaining classes. Added foreign and composite keys. Added comments.

--- Model.cs
+++ Model.cs
@@ -9,6 +9,8 @@ using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.Reflection;
 using System.Diagnostics.SymbolStore;
+using System.Numerics;
+using System.ComponentModel.DataAnnotations.Schema;
 
 //Create new class for database context
 public class EventContext : DbContext
@@ -47,31 +49,29 @@ public class EventContext : DbContext
 
 }
 
-public class Event                             //Stock class containing all information for stock
+public class Event                                      //Event Entity Table
 {
-    //Ignore so that the csvReader does not try and use it as a column to read into. Key marks as unique identifier for entry.
-    [Key] public Guid EventId { get; set; }              //Identifier for Stock entry.
+    [Key] public Guid EventId { get; set; }             
     public string Name { get; set; }
     public string Description { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
     public string Website {  get; set; }
-    public Location Location { get; set; }
-
+    public Location Location { get; set; }              //FK to Location Table
 }
 
-public class Location
+public class Location                                   //Location Entity Table
 {
     [Key] public string Address { get; set; }
     public string Website { get; set; }
     public string Email { get; set; }
-    public float RentalFee { get; set; }
+    public decimal RentalFee { get; set; }
     public int VendorCapacity { get; set; }
     public int AttendeeCapacity { get; set; }
     public List<Event> Events { get; set; }
 }
 
-public class Attendee
+public class Attendee                                   //Attendee Entity Table
 {
     [Key] public Guid AttendeeID { get; set; }
     public string Name { get; set; }
@@ -80,49 +80,87 @@ public class Attendee
     public DateTime CheckinTime { get; set; }
 }
 
-public class Ticket
+public class Ticket                                     //Ticket Entity Table
 {
     [Key] public Guid TicketID { get; set; }
     public float Cost { get; set; }
-    public Attendee Attendee { get; set; }
-    public Event Event { get; set; }
+    public Attendee Attendee { get; set; }              //FK to Attendee Table
+    public Event Event { get; set; }                    //FK to Event Table
     public string TicketType { get; set; }
 }
 
-public class Host
+public class Host                                       //Host Parent Entity Table
 {
     [Key] public Guid HostID { get; set; }
     public string Website { get; set; }
     public string Email { get; set; }
 }
 
-public class Person
+public class Person                                     /*Person, inherits parent table Host*/
+    : Host
 {
-    [Key] public Guid HostID { get; set; }
-    public string Website { get; set; }
-    public string Email { get; set; }
+    public string Name { get; set; }
+    public string PhoneNum { get; set; }
 }
 
-public class HasSpace
+public class Organization                               /*Organization, inherits parent table Host*/
+    :Host
 {
-    [Key] public int roomID { get; set; }
-    [Key] public int TableID { get; set; }
-    public Vendor Vendor { get; set; }
-    public Event Event { get; set; }
+    public string OrganizationName { get; set; }                //Name of organization
+    public string OrganizationPhone { get; set; }
+    public string RepresentativeName { get; set; }      //Representative name
+    public string RepresentativePhone { get; set; }
 }
 
-public class HostedBy
+[PrimaryKey(nameof(RoomID), nameof(TableID))]           //Composite primary key for HasSpace
+public class HasSpace                                   //HasSpace Multi-Multi Relationship Table
 {
-    [Key] public Event Event { get; set; }
-    [Key] public Host Host { get; set; }
+    public int RoomID { get; set; }
+    public int TableID { get; set; }
+    public Vendor Vendor { get; set; }                  //FK for Vendor
+    public Event Event { get; set; }                    //FK for Event
 }
 
-public class Employee
+public class HostedBy                                   //HostedBy Multi-Multi Relationship Table
+{
+    [Key] public Event Event { get; set; }              //FK for Event
+    [Key] public Host Host { get; set; }                //FK for Host
+}
+
+public class Employee                                   //Employee Entity Table
 {
     [Key] public Guid EmpID { get; set;}
     public string Name { get; set; }
-    public float Pay { get; set; }
-    public string ShiftSchedule { get; set; }
-    public string WorkAddress { get; set; }
-    public Host Host { get; set; }
+    public decimal Pay { get; set; }
+    public List<Tuple<DateTime, DateTime>> ShiftSchedule { get; set; }      //May have problems
+    public Location Location { get; set; }              //FK to location of workplace if applicable
+    public Host Host { get; set; }                      //FK to host they work for if applicable
+}
+
+public class Presenter                                  //Presenter Entity Table
+{
+    [Key] public Guid PresenterID { get; set; }     //Guid key
+    public string Name { get; set; }                //Name of presenter
+    public string Email { get; set; }               //email of presenter
+    public string PhoneNum { get; set; }            //Phone number of presenter
+    public decimal Fee { get; set; }                //Fee to present (+ = presenter pays, - = event pays presenter)
+}
+
+[PrimaryKey(nameof(RoomID), nameof(Time))]              //Composite Primary Key for Presents. Can't have two presenters in the same room simultaneously
+public class Presents                                   //Presents Multi-Multi Relationship Table
+{
+    public Presenter Presenter { get; set; }        //FK to Presenter
+    public Event Event { get; set; }                //FK to Event
+    public string RoomID { get; set; }              //Presentation room
+    public string Title { get; set; }               //Title of presentation
+    public string Description { get; set; }         //Description of presentation
+    public DateTime Time { get; set; }              //Time of presentation
+}
+public class Vendor                                     //Vendor Entity Table
+{
+    [Key] Guid VendorID { get; set; }               //ID of vendor
+    public string Name { get; set; }                //Name of vendor
+    public string Email { get; set; }               //Email of vendor
+    public string PhoneNum { get; set; }            //Phone number of vendor
+    public decimal Fee { get; set; }                //Fee for space (charged to vendor)
 }
