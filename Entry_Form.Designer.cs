87d732c5950a4b3322bd79b056b401169d8c0659
HeWhoWalksBefore <dviera@usf.edu>
2023-11-01 19:36:16 -0400

    Created basic log in form

--- Entry_Form.Designer.cs
+++ Entry_Form.Designer.cs
@@ -28,19 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel1 = new Panel();
+            label2 = new Label();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            panel2 = new Panel();
+            comboBox1 = new ComboBox();
+            label3 = new Label();
+            button2 = new Button();
+            panel3 = new Panel();
+            richTextBox1 = new RichTextBox();
+            panel4 = new Panel();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            panel4.SuspendLayout();
             SuspendLayout();
             // 
-            // Form1
+            // panel1
+            // 
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(button1);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 131);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(283, 205);
+            panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.None;
+            label2.AutoSize = true;
+            label2.Location = new Point(22, 90);
+            label2.Name = "label2";
+            label2.Size = new Size(87, 25);
+            label2.TabIndex = 4;
+            label2.Text = "Password";
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.None;
+            label1.AutoSize = true;
+            label1.Location = new Point(18, 53);
+            label1.Name = "label1";
+            label1.Size = new Size(91, 25);
+            label1.TabIndex = 3;
+            label1.Text = "Username";
+            // 
+            // textBox2
+            // 
+            textBox2.Anchor = AnchorStyles.None;
+            textBox2.Location = new Point(115, 87);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(150, 31);
+            textBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.None;
+            textBox1.Location = new Point(115, 50);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 31);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.None;
+            button1.Location = new Point(82, 124);
+            button1.Name = "button1";
+            button1.Size = new Size(130, 40);
+            button1.TabIndex = 0;
+            button1.Text = "Log In";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(comboBox1);
+            panel2.Controls.Add(label3);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(283, 131);
+            panel2.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            comboBox1.Anchor = AnchorStyles.None;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(50, 65);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(182, 33);
+            comboBox1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.None;
+            label3.AutoSize = true;
+            label3.Location = new Point(98, 32);
+            label3.Name = "label3";
+            label3.Size = new Size(87, 25);
+            label3.TabIndex = 0;
+            label3.Text = "Log In As";
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.None;
+            button2.Location = new Point(70, 241);
+            button2.Name = "button2";
+            button2.Size = new Size(130, 60);
+            button2.TabIndex = 5;
+            button2.Text = "Register New User";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(button2);
+            panel3.Controls.Add(richTextBox1);
+            panel3.Dock = DockStyle.Left;
+            panel3.Location = new Point(0, 0);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(270, 336);
+            panel3.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Anchor = AnchorStyles.None;
+            richTextBox1.Location = new Point(9, 35);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(252, 96);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "New Users, click here to log in.\nOtherwise, use the right side to log in to an existing account.";
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(panel1);
+            panel4.Controls.Add(panel2);
+            panel4.Dock = DockStyle.Fill;
+            panel4.Location = new Point(270, 0);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(283, 336);
+            panel4.TabIndex = 3;
+            // 
+            // Entry_Form
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Name = "Form1";
+            ClientSize = new Size(553, 336);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Name = "Entry_Form";
             Text = "Form1";
-            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel4.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Button button1;
+        private Panel panel2;
+        private Label label2;
+        private Label label1;
+        private Button button2;
+        private ComboBox comboBox1;
+        private Label label3;
+        private Panel panel3;
+        private Panel panel4;
+        private RichTextBox richTextBox1;
     }
 }
