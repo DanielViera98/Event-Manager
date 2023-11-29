82ec5c945e51c2f6cbacaa07093eb7a3c5d8b031
DanielViera98 <dviera98@gmail.com>
2023-11-28 19:25:33 -0500

    updated designer

--- LocationItems/LocationLanding.Designer.cs
+++ LocationItems/LocationLanding.Designer.cs
@@ -31,7 +31,9 @@
             dataGridView_Locations = new DataGridView();
             button_addLocation = new Button();
             button_Refresh = new Button();
+            panel1 = new Panel();
             ((System.ComponentModel.ISupportInitialize)dataGridView_Locations).BeginInit();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // dataGridView_Locations
@@ -42,12 +44,12 @@
             dataGridView_Locations.Name = "dataGridView_Locations";
             dataGridView_Locations.RowHeadersWidth = 62;
             dataGridView_Locations.RowTemplate.Height = 33;
-            dataGridView_Locations.Size = new Size(800, 225);
+            dataGridView_Locations.Size = new Size(800, 344);
             dataGridView_Locations.TabIndex = 0;
             // 
             // button_addLocation
             // 
-            button_addLocation.Location = new Point(545, 298);
+            button_addLocation.Location = new Point(403, 18);
             button_addLocation.Name = "button_addLocation";
             button_addLocation.Size = new Size(110, 70);
             button_addLocation.TabIndex = 1;
@@ -57,7 +59,7 @@
             // 
             // button_Refresh
             // 
-            button_Refresh.Location = new Point(381, 298);
+            button_Refresh.Location = new Point(287, 18);
             button_Refresh.Name = "button_Refresh";
             button_Refresh.Size = new Size(110, 70);
             button_Refresh.TabIndex = 2;
@@ -65,17 +67,27 @@
             button_Refresh.UseVisualStyleBackColor = true;
             button_Refresh.Click += button_Refresh_Click;
             // 
+            // panel1
+            // 
+            panel1.Controls.Add(button_Refresh);
+            panel1.Controls.Add(button_addLocation);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 344);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 106);
+            panel1.TabIndex = 3;
+            // 
             // LocationLanding
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button_Refresh);
-            Controls.Add(button_addLocation);
+            Controls.Add(panel1);
             Controls.Add(dataGridView_Locations);
             Name = "LocationLanding";
             Text = "LocationLanding";
             ((System.ComponentModel.ISupportInitialize)dataGridView_Locations).EndInit();
+            panel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -84,5 +96,6 @@
         private DataGridView dataGridView_Locations;
         private Button button_addLocation;
         private Button button_Refresh;
+        private Panel panel1;
     }
 }
